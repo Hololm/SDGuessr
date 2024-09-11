f9835b0982970e08d5d171a437a5a5d0701172c7
Hololm <joseph.holm27@gmail.com>
2024-09-10 22:24:55 -0700

    clickable marker with info functionality

--- sdguessr/src/components/GoogleMap.tsx
+++ sdguessr/src/components/GoogleMap.tsx
@@ -23,25 +23,54 @@ function GoogleMap() {
 
       const map = new Map(document.getElementById('map') as HTMLElement, {
         center: { lat: 34.002866, lng: -111.782272 },
-        zoom: 8,
+        zoom: 7,
+        rotateControl: false,
+        streetViewControl: false,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+          style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+          mapTypeIds: ["roadmap", "satellite", "hybrid"],
+        },
         gestureHandling: 'greedy',
         mapId: '5ef5eba7b54980de',
+
+        restriction: {
+          latLngBounds:
+              {north: 50.35, south: 20.89, west: -125, east: -71.76,},
+          strictBounds: false,
+          },
       });
+
 
       // Marker Function
       const { AdvancedMarkerElement } = await google.maps.importLibrary('marker') as google.maps.MarkerLibrary;
 
-      const marker = new AdvancedMarkerElement({
-        map, // Attach marker to map
-        position: { lat: 34.002866, lng: -111.782272 }, // Position the marker
-      });
+          const secretMessages = ["donovan", "is", "the", "secret", "message"];
+          function attachSecretMessage(
+              marker: google.maps.marker.AdvancedMarkerElement,
+              secretMessage: string
+          ) {
+            const infowindow = new google.maps.InfoWindow({
+              content: secretMessage,
+            });
 
-      // Add a click listener to the marker
-      // @ts-expect-error
-      marker.addListener('click', ({ domEvent, latLng }) => {
-        console.log('Marker clicked at:', latLng.toString());
-        // Add more logic here if needed
-      });
+            marker.addListener("click", () => {
+              infowindow.open(marker.map, marker);
+            });
+          }
+
+      let markers: google.maps.Marker[] = [];
+      map.addListener("click", (event: google.maps.MapMouseEvent) => {
+      addMarker(event.latLng!);
+       });
+
+      function addMarker(position: google.maps.LatLng | google.maps.LatLngLiteral) {
+        const marker = new google.maps.Marker({
+          position,
+          map,
+        });
+        attachSecretMessage(marker, secretMessages[0]);
+      }
 
     };
 
